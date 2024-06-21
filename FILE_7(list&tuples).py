a5b0e9d1bf84d809d1d179be1c627c5051415aae
VaibhavGIT5048 <vaibhav.gupta5048@gmail.com>
2024-06-22 00:11:27 +0530

    .

---
+++ FILE_7(list&tuples).py
@@ -0,0 +1,29 @@
+L2 = ['Python', 'C++', 'Java', 'Ruby']
+
+# Sorting the list
+L2.sort()
+print("After sort:", L2)
+
+# Reversing the list
+L2.reverse()
+print("After reverse:", L2)
+
+# Popping the last element
+L2.pop()
+print("After pop:", L2)
+
+# Sorting the list in reverse order
+L2.sort(reverse=True)
+print("After reverse sort:", L2)
+
+# Removing 'Python' from the list
+L2.remove('Python')
+print("After removing 'Python':", L2)
+
+# Inserting 'R' at index 3
+L2.insert(3, 'R')
+print("After inserting 'R' at index 3:", L2)
+
+# Appending 'mongoDB' to the list
+L2.append('mongoDB')
+print("After appending 'mongoDB':", L2)
