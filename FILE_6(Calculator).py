3974af5fb2d6e8657fcf421761def303918b0f6f
VaibhavGIT5048 <vaibhav.gupta5048@gmail.com>
2024-06-21 20:38:14 +0530

    .

--- FILE_6(Calculator).py
+++ FILE_6(Calculator).py
@@ -2,7 +2,7 @@ str1= "Welcome To Calculator"
 print(str1.center(50).upper())
 A= int(input("Enter The First Number  "))
 B= int(input("Enter The Second Number  "))
-Operation= input("Enter Your operation")
+Operation= input("Enter Your operation  ")
 
 if Operation=="+":
     print('The sum is', A+B)
